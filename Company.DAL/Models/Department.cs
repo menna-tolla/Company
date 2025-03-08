18efaf72a125d14d7626a4911d6f93b08fea7303
ACER <ACER@DESKTOP-7AQ5ACS>
2025-03-08 17:01:09 +0200

    Implementation of DAL

---
+++ Company.DAL/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.DAL.Models
+{
+    class Department
+    {
+        public int Id { set; get; }
+
+        public string Code { set; get; }
+
+        public string Name { set; get; }
+
+        public DateTime CreatAt { set; get; }
+
+
+    }
+}
