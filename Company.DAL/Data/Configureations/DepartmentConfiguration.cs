9c04e5b406f4c140f59eb422f934f90fba6415d5
ACER <ACER@DESKTOP-7AQ5ACS>
2025-03-09 04:22:27 +0200

    Department Module Bll

--- Company.DAL/Data/Configureations/DepartmentConfiguration.cs
+++ Company.DAL/Data/Configureations/DepartmentConfiguration.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Company.DAL.Data.Configureations
 {
-    class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+   public class DepartmentConfiguration : IEntityTypeConfiguration<Department>
     {
         public void Configure(EntityTypeBuilder<Department> builder)
         {
