8913169be94f0539a4187ab704294a7951b9bb9b
ACER <ACER@DESKTOP-7AQ5ACS>
2025-03-19 14:15:16 +0200

    Implementation Of BLL For Employee Service Before Genaric

---
+++ Company.PLL/Interfaces/IEmplyeeRepository.cs
@@ -0,0 +1,22 @@
+﻿using Company.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.BLL.Interfaces
+{
+    public interface IEmplyeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+
+        Employee? Get(int Id);
+
+        int Add(Employee department);
+
+        int Update(Employee department);
+
+        int Delete(Employee department);
+    }
+}
---
+++ Company.PLL/Repositores/EmplyeeRepository.cs
@@ -0,0 +1,53 @@
+﻿using Company.BLL.Interfaces;
+using Company.DAL.Data.Contexts;
+using Company.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.BLL.Repositores
+{
+    class EmplyeeRepository : IEmplyeeRepository
+    {
+        private readonly CompanyDbContext _context;
+
+        public EmplyeeRepository(CompanyDbContext context)
+        {
+           _context = context;
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _context.Employees.ToList();
+        }
+
+        public Employee? Get(int id)
+        {
+            return _context.Employees.Find(id);
+        }
+        
+        public int Add(Employee model)
+        {
+            _context.Employees.Add(model);
+            return _context.SaveChanges();
+        }
+
+        public int Update(Employee model)
+        {
+            _context.Employees.Update(model);
+            return _context.SaveChanges();
+        }
+        public int Delete(Employee model)
+        {
+            _context.Employees.Remove(model);
+            return _context.SaveChanges();
+        }
+
+      
+
+       
+    }
+}
